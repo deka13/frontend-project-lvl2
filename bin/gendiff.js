111db3ba34435df921cc6482911f69639c89eab7
Denis Klyuev <deka.klyuev@mail.ru>
2021-05-02 08:33:04 +0700

    Add option and arguments flags for gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 import program from 'commander';
 
-program.description('Compares two configuration files and shows a difference.').version('0.0.1');
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
