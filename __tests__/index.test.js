0691f3860c6bdc4b5f497e81d6f17cf01fd2b9eb
Denis Klyuev <deka.klyuev@mail.ru>
2021-05-10 12:56:24 +0700

    Add first test

---
+++ __tests__/index.test.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@jest/globals';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import makeDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFilePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getExpectedResult = (fileName) => fs.readFileSync(getFilePath(fileName), 'utf-8').trim();
+
+describe('Test default format', () => {
+  test('Flat json files', () => {
+    const filePath1 = './__fixtures__/file1.json';
+    const filePath2 = './__fixtures__/file2.json';
+    const expected = getExpectedResult('stylishDiff.txt');
+    expect(makeDiff(filePath1, filePath2)).toBe(expected);
+  });
+});
