1d21b7117a69ffa430f87803348a9b15ea7695e8
Denis Klyuev <deka.klyuev@mail.ru>
2021-06-27 20:55:14 +0700

    Changed getDiff.js

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,23 +1,26 @@
 import _ from 'lodash';
 
-const getDiff = (data1, data2) => {
-  const keys = _.union(_.keys(data1), _.keys(data2));
+const getDiff = (parsedData1, parsedData2) => {
+  const keys = _.union(_.keys(parsedData1), _.keys(parsedData2));
   const sortedKeys = _.sortBy(keys);
-  const diff = sortedKeys
-    .map((key) => {
-      if (!_.has(data2, key)) {
-        return `  - ${key}: ${data1[key]}`;
-      }
-      if (!_.has(data1, key)) {
-        return `  + ${key}: ${data2[key]}`;
-      }
-      if (data1[key] !== data2[key]) {
-        return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
-      }
-      return `    ${key}: ${data1[key]}`;
-    })
-    .join('\n');
-  return `{\n${diff}\n}`;
+  const diff = sortedKeys.map((key) => {
+    if (!_.has(parsedData2, key)) {
+      return { type: 'deleted', key, value: parsedData1[key] };
+    }
+    if (!_.has(parsedData1, key)) {
+      return { type: 'added', key, value: parsedData2[key] };
+    }
+    if (_.isObject(parsedData1[key]) && _.isObject(parsedData2[key])) {
+      return { type: 'nested', key, children: getDiff(parsedData1[key], parsedData2[key]) };
+    }
+    if (parsedData1[key] !== parsedData2[key]) {
+      return {
+        type: 'updated', key, newValue: parsedData2[key], oldValue: parsedData1[key],
+      };
+    }
+    return { type: 'unchanged', key, value: parsedData2[key] };
+  });
+  return diff;
 };
 
 export default getDiff;
